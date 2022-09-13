d50690266b51a0339e81ef5028871d320a49fb10
Claire Cashmore <clairecashmore2017@gmail.com>
2022-09-13 14:55:22 -0400

    added get and post functionality

--- client/src/js/index.js
+++ client/src/js/index.js
@@ -1,5 +1,3 @@
-import "./form";
-import "./submit";
 import Logo from "../images/logo.png";
 import Bear from "../images/bear.png";
 import Dog from "../images/dog.png";
@@ -11,12 +9,55 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "../css/index.css";
 
 //importing db
-import { initdb } from "./database";
+import { initdb, getDb, postDb } from "./database";
+// card import
+import { fetchCards } from "./cards";
+//form import
+import { toggleForm, clearForm } from "./form";
 
 window.addEventListener("load", function () {
   initdb();
+  getDb();
+  postDb("Claire", "claire@gmail.com", "5555555555", "Gon");
+  getDb();
+  fetchCards();
   document.getElementById("logo").src = Logo;
   document.getElementById("bearThumbnail").src = Bear;
   document.getElementById("dogThumbnail").src = Dog;
   document.getElementById("gonThumbnail").src = Gon;
+});
+
+// Form functionality
+const form = document.getElementById("formToggle");
+const newContactButton = document.getElementById("new-contact");
+let submitBtnToUpdate = false;
+let profileId;
+
+newContactButton.addEventListener("click", (event) => {
+  toggleForm();
+});
+
+form.addEventListener("submit", (event) => {
+  // Handle data
+  event.preventDefault();
+  let name = document.getElementById("name").value;
+  let phone = document.getElementById("phone").value;
+  let email = document.getElementById("email").value;
+  let profile = document.querySelector('input[type="radio"]:checked').value;
+
+  // Post form data to IndexedDB OR Edit an existing card in IndexedDB
+  if (submitBtnToUpdate == false) {
+    postDb(name, email, phone, profile);
+  } else {
+    fetchCards();
+    // Toggles the submit button back to POST functionality
+    submitBtnToUpdate = false;
+  }
+
+  // Clear form
+  clearForm();
+  // Toggle form
+  toggleForm();
+  // Reload the DOM
+  fetchCards();
 });
