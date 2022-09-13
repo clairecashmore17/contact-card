4bbdc8adab7eb926531ff751ec60067f0489da4c
Claire Cashmore <clairecashmore2017@gmail.com>
2022-09-13 13:57:21 -0400

    created contact_db table in indexeddb

--- client/src/js/index.js
+++ client/src/js/index.js
@@ -10,7 +10,11 @@ import "bootstrap/dist/css/bootstrap.min.css";
 //CSS
 import "../css/index.css";
 
+//importing db
+import { initdb } from "./database";
+
 window.addEventListener("load", function () {
+  initdb();
   document.getElementById("logo").src = Logo;
   document.getElementById("bearThumbnail").src = Bear;
   document.getElementById("dogThumbnail").src = Dog;
