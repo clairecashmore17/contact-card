43dfa70c4c77d9c0f5d80ff688a6b540dbe0190b
Claire Cashmore <clairecashmore2017@gmail.com>
2022-09-13 12:24:20 -0400

    bundled css

--- client/src/js/index.js
+++ client/src/js/index.js
@@ -1,2 +1,18 @@
 import "./form";
 import "./submit";
+import Logo from "../images/logo.png";
+import Bear from "../images/bear.png";
+import Dog from "../images/dog.png";
+import Gon from "../images/gon.jpg";
+//bootstrap imports
+import { Tooltip, Toast, Popover } from "bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+//CSS
+import "../css/index.css";
+
+window.addEventListener("load", function () {
+  document.getElementById("logo").src = Logo;
+  document.getElementById("bearThumbnail").src = Bear;
+  document.getElementById("dogThumbnail").src = Dog;
+  document.getElementById("gonThumbnail").src = Gon;
+});
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -3,10 +3,34 @@ const path = require("path");
 module.exports = {
   // sets our starting file to this as our dependency graph
   entry: "./src/js/index.js",
-  //output is where the budled files will be generated, we set it to main.js
+  //output is where the budled files will be generated, we set it to bundle.js
   output: {
-    filename: "main.js",
+    filename: "bundle.js",
     //path sets our directory where the bundled files will be outputted, which we call and send to dist
     path: path.resolve(__dirname, "dist"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
+      //css files to be handled by the specified loaders
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      //js files to be handled by babel
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: [["@babel/preset-env", { targets: "defaults" }]],
+          },
+        },
+      },
+    ],
   },
 };
