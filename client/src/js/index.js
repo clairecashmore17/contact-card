91034768c6a9df678ed481cd15393e721c43874c
Claire Cashmore <clairecashmore2017@gmail.com>
2022-09-14 19:25:57 -0400

    added manifest.json

--- client/src/js/index.js
+++ client/src/js/index.js
@@ -28,8 +28,25 @@ window.addEventListener("load", function () {
 // Form functionality
 const form = document.getElementById("formToggle");
 const newContactButton = document.getElementById("new-contact");
+const installBtn = document.getElementById(‘installBtn’);
 let submitBtnToUpdate = false;
 let profileId;
+
+//install button
+window.addEventListener('beforeinstallprompt', (event) => {
+    event.preventDefault();
+    installBtn.style.visibility = 'visible';
+    
+    installBtn.addEventListener('click', () => {
+        event.prompt();
+        installBtn.setAttribute('disabled', true);
+        installBtn.textContent = 'Installed!';
+        });
+});
+window.addEventListener('appinstalled', (event) => {
+    console.log('👍', 'appinstalled', event);
+  });
+
 
 newContactButton.addEventListener("click", (event) => {
   toggleForm();
